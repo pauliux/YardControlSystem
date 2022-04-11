0c6552008d929ab421861ad3dc32e895a4bd5bd7
Paulius <pauliusgil@gmail.com>
2022-04-11 03:05:53 +0300

    Add project files.

---
+++ InAndOut/Controllers/Manager/WarehouseController.cs
@@ -0,0 +1,116 @@
+﻿using InAndOut.Data;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YardControlSystem.Models;
+
+namespace YardControlSystem.Controllers
+{
+    public class WarehouseController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public WarehouseController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+ 
+        public IActionResult WarehousesView()
+        {
+            IEnumerable<Warehouse> objList = _db.Warehouses;
+            return View(objList);
+
+        }
+
+        // GET-Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Warehouse obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Warehouses.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("WarehousesView");
+            }
+            return View(obj);
+
+        }
+
+
+        // GET Delete
+        public IActionResult Delete(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Warehouses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        // POST Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Warehouses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            _db.Warehouses.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("WarehousesView");
+
+        }
+
+        // GET Update
+        public IActionResult Update(int? id)
+        {
+
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Warehouses.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+
+        }
+
+        // POST UPDATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(Warehouse obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Warehouses.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("WarehousesView");
+            }
+            return View(obj);
+
+        }
+    }
+}
---
+++ InAndOut/Migrations/20220410185845_addWarehousesToDatabase.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace InAndOut.Migrations
+{
+    public partial class addWarehousesToDatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Warehouses",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Company = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    City = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Warehouses", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Warehouses");
+        }
+    }
+}

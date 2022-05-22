716ea40f097165f6c4bad78dbfcbdde010c44129
GytisBurbeckas <gytis.burbeckas@ktu.edu>
2022-05-22 20:10:51 +0300

    StorekeeperRamp
    Warehouse, ramp, operations perziurejimas(Pirmas bandymas)

---
+++ YardControlSystem/Controllers/StoreKeeper/RampController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using YardControlSystem.Data;
+using YardControlSystem.Models;
+using YardControlSystem.Models.ViewModels;
+
+
+
+
+namespace YardControlSystem.Controllers.StoreKeeper
+{
+    public class RampController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public RampController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        // GET: OrderController
+        public IActionResult RampManagementView()
+        {
+            var warehouses = _db.Warehouses.ToList();
+            var ramps = _db.Ramps.ToList();
+            var operations = _db.Operations.ToList();
+
+            var warehouseRampOperation = new List<RampManagementViewModel>();
+
+            foreach (var oneWarehouse in warehouses)
+            {
+                List<Ramp> rampList = new List<Ramp>();
+                List<Operation> operationList = new List<Operation>();
+                foreach (var oneRamp in ramps)
+                {
+                    if (oneWarehouse.Id == oneRamp.WarehouseId)
+                    {
+                        rampList.Add(oneRamp);
+                    }
+
+                   
+
+                    foreach (var oneOperation in operations)
+                    {
+                        if (oneRamp.Id == oneOperation.RampId)
+                        {
+                            operationList.Add(oneOperation);
+                        }
+                    }
+                }
+                warehouseRampOperation.Add(new RampManagementViewModel
+                {
+                    Warehouses = oneWarehouse,
+                    Ramps = rampList,
+                    Operations = operationList
+
+                }) ;
+            }
+
+            return View(warehouseRampOperation);
+        }
+    }
+}

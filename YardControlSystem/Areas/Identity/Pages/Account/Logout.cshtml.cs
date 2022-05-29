6b2eed913b673d0bbe9d7a1017d0c741eae649c6
Paulius <pauliusgil@gmail.com>
2022-05-29 17:22:51 +0300

    Redirect to login after logout

--- YardControlSystem/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ YardControlSystem/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -33,7 +33,7 @@ namespace YardControlSystem.Areas.Identity.Pages.Account
             _logger.LogInformation("User logged out.");
             if (returnUrl != null)
             {
-                return LocalRedirect(returnUrl);
+                return Redirect("Login");
             }
             else
             {
